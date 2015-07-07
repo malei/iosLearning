4c48697ffbf814aa8ab941186c00e77c8fc66069
malei <maleih@gmail.com>
2015-07-07 18:33:38 +0800

    add test_iphone proj

---
+++ test_iphone/test_iphone/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test_iphone
+//
+//  Created by malei on 7/6/15.
+//  Copyright (c) 2015 malei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test_iphone/test_iphone/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test_iphone
+//
+//  Created by malei on 7/6/15.
+//  Copyright (c) 2015 malei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
