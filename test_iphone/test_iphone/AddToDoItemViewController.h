36aff8bf7396ff8af1ef04d57ef8c7eb139b6208
malei <maleih@gmail.com>
2015-07-09 13:29:15 +0800

    Fix test_iphone

---
+++ test_iphone/test_iphone/AddToDoItemViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddToDoItemViewController.h
+//  test_iphone
+//
+//  Created by malei on 7/8/15.
+//  Copyright (c) 2015 malei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddToDoItemViewController : UIViewController
+
+@end
---
+++ test_iphone/test_iphone/ToDoListTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  ToDoListTableViewController.h
+//  test_iphone
+//
+//  Created by malei on 7/8/15.
+//  Copyright (c) 2015 malei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ToDoListTableViewController : UITableViewController
+
+- (IBAction)unwindToList:(UIStoryboardSegue *)segue;
+
+
+@end
